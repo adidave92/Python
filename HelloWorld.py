22440c5c02f34a94864b3f2ad6f894f0571c1321
Aditya <aditya.dave92@gmail.com>
2018-02-16 15:41:16 -0500

    First Commit

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+x='Aditya'
+while True:
+    x=input('\n Whats your name? \n')
+    if x=="Aditya":
+        print("\n Welcome Aditya, lets begin!!")
+        break
+    else:
+        print('\n Access Denied, Try Again \n')
+
